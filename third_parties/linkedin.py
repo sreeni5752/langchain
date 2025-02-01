8ea5d277b73ff881151043229cf47a4734d09bb9
Eden Marco <emarco177@gmail.com>
2025-02-01 12:24:32 +0200

    updated to use scrapin

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -1,26 +1,47 @@
 import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-def scrape_linkedin_profile(linkedin_profile_url: str):
+def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
     """scrape information from LinkedIn profiles,
     Manually scrape the information from the LinkedIn profile"""
-    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
-    header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
 
-    response = requests.get(
-        api_endpoint, params={"url": linkedin_profile_url}, headers=header_dic
-    )
+    if mock:
+        linkedin_profile_url = "https://gist.githubusercontent.com/emarco177/859ec7d786b45d8e3e3f688c6c9139d8/raw/32f3c85b9513994c572613f2c8b376b633bfc43f/eden-marco-scrapin.json"
+        response = requests.get(
+            linkedin_profile_url,
+            timeout=10,
+        )
+    else:
+        # ENROLL WITH COUPON CODE: EDENMARCO
+        # For 20% Discount on all pricing
+        api_endpoint = "https://api.scrapin.io/enrichment/profile"
+        params = {
+            "apikey": os.environ["SCRAPIN_API_KEY"],
+            "linkedInUrl": linkedin_profile_url,
+        }
+        response = requests.get(
+            api_endpoint,
+            params=params,
+            timeout=10,
+        )
 
-    data = response.json()
+    data = response.json().get("person")
     data = {
         k: v
         for k, v in data.items()
-        if v not in ([], "", "", None)
-        and k not in ["people_also_viewed", "certifications"]
+        if v not in ([], "", "", None) and k not in ["certifications"]
     }
-    if data.get("groups"):
-        for group_dict in data.get("groups"):
-            group_dict.pop("profile_pic_url")
 
     return data
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/eden-marco/"
+        ),
+    )
